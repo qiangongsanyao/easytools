31512ce22438b3e88e84c28660a7da5d8b56c702
ch <2790870823@qq.com>
2018-02-22 09:21:21 +0800

    init

---
+++ easytools/src/main/java/easytools/ch/App.java
@@ -0,0 +1,23 @@
+package easytools.ch;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+	static Logger logger = LogManager.getLogger(App.class);
+	
+    public static void main( String[] args ){
+    	logger.debug("debug");
+    	logger.info("info");
+    	logger.error("error");
+    	logger.warn("warn");
+    	logger.trace("trace");
+    	logger.fatal("fatal");
+    }
+    
+}
---
+++ easytools/src/main/java/easytools/ch/MyController.java
@@ -0,0 +1,23 @@
+package easytools.ch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class MyController {
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String say() {
+		return "Hello World!";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MyController.class, args);
+	}
+	
+}
