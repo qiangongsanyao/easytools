abc77b41b77a85fd71f36c864404e5c2da6aef91
ch <2790870823@qq.com>
2018-02-22 15:56:10 +0800

    annotion

--- easytools/src/main/java/easytools/ch/bean/converter/BeanConverter.java
+++ easytools/src/main/java/easytools/ch/bean/converter/BeanConverter.java
@@ -1,5 +1,10 @@
 package easytools.ch.bean.converter;
 
+/**
+ * conerter a javabean to targetjavabean.<br>
+ * @author Ch
+ *
+ */
 public interface BeanConverter {
 
 	public <T> T convert(Object object,Class<T> targetClass);
